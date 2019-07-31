17cc46734f7313619190d9d084b7b37daab6fd5d
anthonyalchemy <anthony@alchemytechs.com>
2019-07-31 12:20:49 +0100

    Add project files.

---
+++ Comments/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Comments are for programmers or developers 
+
+            /* Mulitple 
+               Line
+               Comment
+            */
+
+
+            Console.WriteLine("Comments are fun");
+
+
+            Console.ReadLine();
+        }
+    }
+}
